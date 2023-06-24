61511c51b7e84f0948833b8b0f6d784caa61cd1c
Mkeeyse <87869635+Mkeeyse@users.noreply.github.com>
2023-06-24 17:48:55 +0000

    use .evn

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from sqlalchemy import create_engine, text
 from functools import wraps
+from database import engine, login_user, get_user_info
 
 app = Flask(__name__)
 app.secret_key = 'qwertyuio'
-
-# Establish a connection to the database
-engine = create_engine("mysql+mysqlconnector://642ky6svrnizgbjw53hg:pscale_pw_oqbUdqC2dOCT8CLH2XmWC2wydOeA4NH68tC5r0TCTSC@aws.connect.psdb.cloud/file_system?ssl_ca=/etc/ssl/cert.pem")
 
 def login_required(f):
     @wraps(f)
@@ -40,19 +37,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        # Execute the login query
-        with engine.connect() as connection:
-            result = connection.execute(
-                text("SELECT * FROM User WHERE user_name = :username AND user_password = :password"),
-                {"username": username, "password": password}
-            )
-            record = result.fetchone()
+        record = login_user(username, password)
 
         if record:
             session['loggedin'] = True
-            session['username'] = record[1]
-            session['id'] = record[0]  # Store the user_id in the session
-            session['is_admin'] = record[5]  # Store the admin status in the session
+            session['username'] = record['username']
+            session['id'] = record['id']  # Store the user_id in the session
+            session['is_admin'] = record['is_admin']  # Store the admin status in the session
             if session['is_admin']:
                 return redirect(url_for('admin'))
             else:
@@ -66,20 +57,9 @@ def login():
 @app.route('/home')
 @login_required
 def home():
-    # Retrieve user information from the server/database
-    with engine.connect() as connection:
-        result = connection.execute(
-            text("SELECT * FROM User WHERE id = :user_id"),
-            {"user_id": session['id']}
-        )
-        record = result.fetchone()
+    user_info = get_user_info(session['id'])
 
-    if record:
-        user_info = {
-            'username': record[1],
-            'department': record[4],
-            'email': record[2],
-        }
+    if user_info:
         return render_template('home.html', user_info=user_info)
     else:
         return "User information not found."
@@ -91,19 +71,13 @@ def admin_login():
         username = request.form["username"]
         password = request.form["password"]
 
-        # Execute the login query
-        with engine.connect() as connection:
-            result = connection.execute(
-                text("SELECT * FROM User WHERE user_name = :username AND user_password = :password"),
-                {"username": username, "password": password}
-            )
-            record = result.fetchone()
+        record = login_user(username, password)
 
-        if record and record[5]:  # Check if user exists and is an admin
+        if record and record['is_admin']:  # Check if user exists and is an admin
             session['loggedin'] = True
-            session['username'] = record[1]
-            session['id'] = record[0]  # Store the user_id in the session
-            session['is_admin'] = record[5]  # Store the admin status in the session
+            session['username'] = record['username']
+            session['id'] = record['id']  # Store the user_id in the session
+            session['is_admin'] = record['is_admin']  # Store the admin status in the session
             return redirect(url_for('admin'))
         else:
             msg = "Invalid username or password. Please try again."
@@ -113,12 +87,7 @@ def admin_login():
 @app.route('/admin')
 @admin_required
 def admin():
-    # Retrieve all users from the server/database
-    with engine.connect() as connection:
-        result = connection.execute(
-            text("SELECT * FROM User")
-        )
-        users = result.fetchall()
+    users = get_user_info()
 
     return render_template('admin.html', users=users)
 
--- database.py
+++ database.py
@@ -1,17 +1,72 @@
 from sqlalchemy import create_engine, text
+import os
 
-# Establish a connection
-engine = create_engine(
-    "mysql+mysqlconnector://642ky6svrnizgbjw53hg:pscale_pw_oqbUdqC2dOCT8CLH2XmWC2wydOeA4NH68tC5r0TCTSC@aws.connect.psdb.cloud/file_system?ssl_ca=/etc/ssl/cert.pem"
-)
+# Retrieve the database connection string from an environment variable
+db_conn = os.getenv('DB_CONN')
 
-# Execute a test query
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM User"))
-    rows = result.fetchall()
+# Remove unwanted quotes from the db_conn variable
+if db_conn is not None:
+    db_conn = db_conn.replace('"', '')
 
-# Convert rows to a list of dictionaries
-result_dict = [dict(row._mapping) for row in rows]
+print("DB_CONN:", repr(db_conn))
 
-# Print the result
-print(result_dict[0])
+if db_conn is None:
+    print("DB_CONN environment variable is not set.")
+else:
+    # Establish a connection to the database
+    engine = create_engine(db_conn)
+
+def login_user(username, password):
+    with engine.connect() as connection:
+        result = connection.execute(
+            text("SELECT * FROM User WHERE user_name = :username AND user_password = :password"),
+            {"username": username, "password": password}
+        )
+        record = result.fetchone()
+
+    if record:
+        user_info = {
+            'id': record[0],
+            'username': record[1],
+            'is_admin': record[5]
+        }
+        return user_info
+    else:
+        return None
+
+def get_user_info(user_id=None):
+    with engine.connect() as connection:
+        if user_id:
+            result = connection.execute(
+                text("SELECT * FROM User WHERE id = :user_id"),
+                {"user_id": user_id}
+            )
+            record = result.fetchone()
+            if record:
+                user_info = {
+                    'id': record[0],
+                    'username': record[1],
+                    'department': record[4],
+                    'email': record[2]
+                }
+                return user_info
+            else:
+                return None
+        else:
+            result = connection.execute(
+                text("SELECT * FROM User")
+            )
+            users = result.fetchall()
+
+            user_info_list = []
+            for record in users:
+                user_info = {
+                    'id': record[0],
+                    'username': record[1],
+                    'department': record[4],
+                    'email': record[2]
+                }
+                user_info_list.append(user_info)
+
+            return user_info_list
+
